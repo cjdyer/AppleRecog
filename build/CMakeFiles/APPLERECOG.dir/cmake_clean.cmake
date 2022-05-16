d79b4884fb84288eaa9904b861187fbf307d7437
Cory <cjdyer0@gmail.com>
2022-05-16 14:57:15 +1200

    first commit

---
+++ build/CMakeFiles/APPLERECOG.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "APPLERECOG"
+  "APPLERECOG.pdb"
+  "CMakeFiles/APPLERECOG.dir/src/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/APPLERECOG.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/APPLERECOG.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for APPLERECOG.
+# This may be replaced when dependencies are built.
